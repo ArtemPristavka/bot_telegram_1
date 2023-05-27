5368b55e2d3ec66594a9b954919e5ddd73f5e857
Arteem Pristavka <artem_pristavka@mail.ru>
2023-05-27 16:07:06 +0300

    deleteed my token

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+
 storage = MemoryStorage()
-bot = Bot(token='6105356535:AAH6-srqCo7uT60W8l2kGSkhzoFLSWOxTLI')
+bot = Bot(token='')  # Сюда вставлять токен бота
 dp = Dispatcher(bot=bot,
                 storage=storage)
-
